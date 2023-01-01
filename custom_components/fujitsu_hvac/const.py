20cb85cea4e62d44b5a667b50b2e2eaa1f80d976
xaro <roberto.poo@gmail.com>
2023-01-01 22:23:39 +0000

    Rename

--- custom_components/fujitsu_hvac/const.py
+++ custom_components/fujitsu_hvac/const.py
@@ -1,11 +1,11 @@
-"""Constants for integration_blueprint."""
+"""Constants for fujitsu_hvac."""
 # Base component constants
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "Fujitsu HVAC"
+DOMAIN = "fujitsu_hvac"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-ISSUE_URL = "https://github.com/custom-components/integration_blueprint/issues"
+ATTRIBUTION = ""
+ISSUE_URL = ""
 
 # Icons
 ICON = "mdi:format-quote-close"
