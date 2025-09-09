09f6e56141240199f00470c7887c70bf31c4056d
Roberto Poo <roberto.poo@gmail.com>
2025-09-08 22:08:20 -0400

    Fix deprecated call

--- custom_components/fujitsu_hvac/__init__.py
+++ custom_components/fujitsu_hvac/__init__.py
@@ -37,7 +37,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setup(entry, "climate")
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate"])
 
     return True
 
